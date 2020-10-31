c5ad895783cec1df9f7c57ba99f4e39202cf2d09
Your name here <ctrumbo@uncc.edu>
2020-10-31 11:16:35 -0400

    added database functionality

---
+++ Modules/data_schema.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const dataSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        uppercase: true,
+        trim: true,
+        unique: true,
+    },
+    value: {
+        type: Number,
+        required: true,
+    },
+    color: {
+        type: String,
+        required: true,
+    }
+}, { collection: 'data'})
+
+module.exports = mongoose.model('data', dataSchema)
--- Server.js
+++ Server.js
@@ -1,20 +1,53 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
+const dataModel = require("./modules/data_schema");
 const app = express();
 const port = 3000;
 
+let url = 'mongodb://localhost:27017/chart_data'
+
+const mongoDBClient = require('mongodb').MongoClient
+
 app.use(cors());
 
-//app.use('/', express.static('public'));
-
-//const budget = require("./budgetData.json");
-
-//app.get('/hello', (req, res) => {
-//    res.send('hello world');
-//});
-
 app.get('/budget', (req, res) => {
-    res.sendFile('budgetData.json', {root: __dirname});
+    mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        dataModel.find({})
+                .then((data) => {
+                    res.json(data);
+                    mongoose.connection.close();
+                })
+                .catch((connectionError) => {
+                    console.log(connectionError)
+                })
+    })
+    .catch((connectionError) =>{
+        console.log(connectionError)
+    })
+});
+
+app.post('/budget', (req, res) => {
+    mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+            .then(() => {
+                var dataStructure = {
+                    title: req.body.title,
+                    value: req.body.value,
+                    color: req.body.color
+                };
+                dataModel.insertMany(dataStructure)
+                    .then((data) => {
+                        res.json(data);
+                        mongoose.connection.close();
+                    })
+                    .catch((connectionError) => {
+                        console.log(connectionError)
+                    })
+            })
+            .catch((connectionError) => {
+                console.log(connectionError);
+            })
 });
 
 app.listen(port, () => {
